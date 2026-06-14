86e057a027ca8c41bc370e0bf1c31aa5ee7b7230
kalyuke <upusderkis@gmail.com>
2026-06-14 16:31:52 -0300

    Nueva animación para el cartel de sobrevivir el año

--- main.cpp
+++ main.cpp
@@ -12,18 +12,24 @@ using namespace std;
 
 int main()
 {
+    int velocidad=50;
     srand(time(0));
     cout<<fixed<<setprecision(2);
     setlocale(LC_ALL,"");
     string nombre[5];
     double patrimonioReal[5];
-    cout<<"====================================="<<endl;
+    rlutil::locate(15, 0);
+    escribirLentoArcoiris("=====================================", velocidad);
     cout<<endl;
-    cout<<"         SOBREVIVIR AL AÑO           "<<endl;
     cout<<endl;
-    cout<<"====================================="<<endl;
+    escribirLentoArcoiris("          SOBREVIVIR AL AÑO          ", velocidad);
     cout<<endl;
-    cout<<" Presione enter para continuar...";
+    cout<<endl;
+    escribirLentoArcoiris("=====================================", velocidad);
+    cout<<endl;
+    cout<<endl;
+    rlutil::setColor(rlutil::WHITE);
+    escribirLento("     Presione [Enter] para jugar", velocidad);
     system("pause > nul");
     menuPrincipal(nombre, patrimonioReal);
 
